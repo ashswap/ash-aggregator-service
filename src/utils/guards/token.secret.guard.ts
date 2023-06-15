c17c06420f46a14bbc5e0c7c6502d86afa1f7bfa
Tom Tran <haitran.ashswap@gmail.com>
2023-06-15 10:51:55 +0000

    Feat/secure api

--- src/utils/guards/token.secret.guard.ts
+++ src/utils/guards/token.secret.guard.ts
@@ -2,19 +2,25 @@ import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { ApiConfigService } from 'src/common/api-config/api.config.service';
 
 @Injectable()
-export class JwtAdminGuard implements CanActivate {
+export class tokenSecretGuard implements CanActivate {
   constructor(private readonly apiConfigService: ApiConfigService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    if (!this.apiConfigService.isTokenSecretEnabled()) {
+      return true;
+    }
     const request = context.switchToHttp().getRequest();
 
-    const jwt = request.jwt;
-
-    const admins = this.apiConfigService.getSecurityAdmins();
-    if (!admins) {
+    const authToken = request.headers['authen-token'];
+    if (!authToken) {
       return false;
     }
 
-    return admins.includes(jwt.address);
+    const tokenSecret = this.apiConfigService.getTokenSecret();
+    if (!tokenSecret) {
+      return false;
+    }
+
+    return tokenSecret === authToken;
   }
 }
